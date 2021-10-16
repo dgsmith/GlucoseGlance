2d8f91686be22519a93333acd27873c76e24ad47
Grayson Smith <d.grayson.smith@gmail.com>
2021-10-15 20:08:17 -0700

    Layout tweaks

--- GlucoseGlance WatchKit Extension/GlucoseGlanceApp.swift
+++ GlucoseGlance WatchKit Extension/GlucoseGlanceApp.swift
@@ -18,20 +18,9 @@ struct GlucoseGlanceApp: App {
     @SceneBuilder var body: some Scene {
         WindowGroup {
             NavigationView {
-                GeometryReader { geo in
-                    ContentView()
-                        .navigationTitle {
-                            if geo.size.width > 324.0 / 2.0 {
-                                Text("Glucose Glance")
-                                    .foregroundColor(options.theme.main)
-                                    .offset(x: -9.5, y: 0)
-                            } else {
-                                Text("Glucose Glance")
-                                    .foregroundColor(options.theme.main)
-                                    .offset(x: -3.5, y: 0)
-                            }
-                        }
-                }
+                ContentView()
+                    .navigationTitle(Text("Glucose Glance"))
+                    .navigationBarTitleDisplayMode(.inline)
             }
         }
     }
